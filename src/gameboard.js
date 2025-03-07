8e04e301830ee6e5168d2ce9d01011f3a228444e
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2025-03-07 19:20:46 +0100

    add gameboard factory with placeShip method

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,11 +1,51 @@
 function createShip (length) {
-
     let numberOfHits = 0;
 
     const hit = () => numberOfHits++;
 
     const isSunk = () => length === numberOfHits ? true : false;
 
-    return { hit, isSunk }
+    return { hit, isSunk, length }
 }
 
+function createGameboad () {
+    const board = [];
+    let attacks = [];
+
+    for (let i = 0; i < 10; i++) {
+        board.push(Array(10));
+    }
+
+    const letterArr = ["A","B","C","D","E","F","G","H","I","J"];
+
+    const calculateX = (coordinate) => coordinate.slice(1) - 1;
+    const calculateY = (coordinate) => letterArr.indexOf(coordinate[0]);
+
+    const placeShip = (firstCoordinate, ship, direction) => {
+        
+        const xCoordinate = calculateX(firstCoordinate);
+        const yCoordinate = calculateY(firstCoordinate);
+        const xEnd = xCoordinate + ship.length;
+        const yEnd = yCoordinate + ship.length;
+    
+        if (xCoordinate < 0 || xEnd > 10 || yCoordinate < 0 || yEnd > 10) {
+            return;
+        }
+
+        if (direction === "horizontal") {
+            for (let i = 0; i < ship.length; i++) {
+                board[yCoordinate][xCoordinate+i] = ship;
+            }
+        }
+
+        if (direction === "vertical") {
+            for(let i = 0; i < ship.length; i++) {
+                board[yCoordinate+i][xCoordinate] = ship;
+            }
+        }
+    }
+
+    return { board, placeShip }
+}
+
+export { createShip, createGameboad };
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,4 +1,4 @@
-import { createShip } from "./gameboard.js"
+import { createShip, createGameboad } from "./gameboard.js"
 
 describe('test ship factory', () => {
   const ship = createShip(1);
@@ -30,3 +30,26 @@ describe('test ship factory', () => {
     expect(ship.isSunk()).toBeFalsy();
   })
 }) 
+
+describe('test gameboard factory', () => {
+  const ship = createShip(2);
+  const gameboard = createGameboad();
+  gameboard.placeShip("B3", ship, "horizontal");
+  gameboard.placeShip("D9", ship, "vertical");
+
+  test('expect board at B3 to be of type object', () => {
+    expect(typeof gameboard.board[1][2]).toBe('object')
+  })
+
+  test('expect board at B4 to be of type object', () => {
+    expect(typeof gameboard.board[1][3]).toBe('object')
+  })
+
+  test('expect board at B5 to be undefined', () => {
+    expect(gameboard.board[1][4]).toBeUndefined();
+  })
+
+  test('expect board at E9 to be of type object', () => {
+    expect(typeof gameboard.board[4][8]).toBe('object');
+  })
+})
